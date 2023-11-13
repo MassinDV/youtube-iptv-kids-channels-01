36d928abc6ca249af18863678645fbc99c28265e
ureplaytv <massindevelopment@gmail.com>
2023-11-13 20:31:49 +0100

    t

--- sh/scripts/ch42.py
+++ sh/scripts/ch42.py
@@ -13,12 +13,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://www.youtube.com/channel/UCROKYPep-UuODNwyipe6JMw/live')
+            print('https://raw.githubusercontent.com/MassinDV/videos/main/kids/asaventurasdesunnybunnies.m3u8')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://www.youtube.com/channel/UCROKYPep-UuODNwyipe6JMw/live')
+            print('https://raw.githubusercontent.com/MassinDV/videos/main/kids/asaventurasdesunnybunnies.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100

7e736918fbca648a05795ca1344f32cbf99bf081
ureplaytv <massindevelopment@gmail.com>
2023-11-13 16:06:05 +0100

    d

--- sh/scripts/ch10.py
+++ sh/scripts/ch10.py
@@ -13,12 +13,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://dl.dropboxusercontent.com/s/icv7mb38uywusgj/morocco-sport-3.m3u8')
+            print('https://raw.githubusercontent.com/MassinDV/yt-listchannel/main/pyjamasquesofficielle.m3u8')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://dl.dropboxusercontent.com/s/icv7mb38uywusgj/morocco-sport-3.m3u8')
+            print('https://raw.githubusercontent.com/MassinDV/yt-listchannel/main/pyjamasquesofficielle.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100

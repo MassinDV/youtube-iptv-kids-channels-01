f6c115cc11831bd647e95d6b29f6c5f92816c258
Massin <40926996+MassinDV@users.noreply.github.com>
2023-11-13 21:27:17 +0100

    Update ch120.sh

--- sh/ch120.sh
+++ sh/ch120.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 ch120.py > ../../live/ch120.m3u8
+python3 ch120.py > ../../live/ch107.m3u8
 
-echo ch9 grabbed
+echo ch120 grabbed
